3f642b2a03b52a524121f0b3d72c484039931662
Nat2194 <nathan.carlier@efrei.net>
2022-03-21 15:04:42 +0100

    Add Relations between Entities

--- api/src/utils/items/items.entity.ts
+++ api/src/utils/items/items.entity.ts
@@ -1,4 +1,14 @@
-import { Entity, PrimaryKey, Property } from '@mikro-orm/core';
+import {
+  Entity,
+  PrimaryKey,
+  Property,
+  ManyToMany,
+  OneToMany,
+  Collection,
+} from '@mikro-orm/core';
+import { Users } from '../users/user.entity';
+import { Event } from '../event/event.entity';
+import { Items_type } from '../items_type/items_type.entity';
 
 @Entity()
 export class Items {
@@ -10,4 +20,15 @@ export class Items {
 
   @Property()
   Stock!: number;
+
+  // Items belongs to Users collection : 1,n items(s) is bought(s) by 1,n user(s)
+  @ManyToMany(() => Users, (user) => user.item)
+  user = new Collection<Users>(this);
+
+  // Items belongs to Event collection : 1,n items(s) is used(s) in 1,n event(s)
+  @ManyToMany(() => Event, (event) => event.item)
+  event = new Collection<Event>(this);
+
+  @OneToMany(() => Items_type, (items_type) => items_type.item)
+  item_type = new Collection<Items_type>(this);
 }
