7bc041f6921ba06cc5765d6e96a027db62b06b34
Nat2194 <nathan.carlier@efrei.net>
2022-03-26 22:33:28 +0100

    régler bug prettier

--- api/src/goodies/goodies.entity.ts
+++ api/src/goodies/goodies.entity.ts
@@ -2,9 +2,9 @@ import { Entity, Property, PrimaryKey } from '@mikro-orm/core';
 
 @Entity()
 export class Goodies {
-    @PrimaryKey()
-    id_goodies!: number;
+  @PrimaryKey()
+  id_goodies!: number;
 
-    @Property()
-    title!: string;
+  @Property()
+  title!: string;
 }
