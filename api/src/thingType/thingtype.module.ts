361bb665538d55a239c79dd39cfadc53ce79f328
Nat2194 <nathan.carlier@efrei.net>
2022-04-30 14:21:12 +0200

    CA MARCHE JE SUIS CONTEEEEEEENT

--- api/src/thingType/thingtype.module.ts
+++ api/src/thingType/thingtype.module.ts
@@ -1,8 +1,10 @@
 import { MikroOrmModule } from '@mikro-orm/nestjs';
 import { Module } from '@nestjs/common';
-import { ThingTypeController } from './thingtype.controller';
-import { ThingTypeService } from './thingtype.service';
+
+//Custom Packages
 import { ThingType } from './thingType.entity';
+import { ThingTypeController } from './thingType.controller';
+import { ThingTypeService } from './thingType.service';
 
 @Module({
   imports: [MikroOrmModule.forFeature({ entities: [ThingType] })],
